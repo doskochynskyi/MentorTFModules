da0f1c1019f703ffb8899f2551990e8fcc75f0a0
Ivan <ivan.doskochynskyi@gmail.com>
2022-06-28 23:49:01 +0300

    add network module

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -1,20 +1,20 @@
 resource "azurerm_virtual_network" "net" {
   name                = "tfvnetnginx"
   address_space       = ["192.168.0.0/16"]
-  location            = ${var.region}
-  resource_group_name = ${var.resource_group}
+  location            = "${var.region}"
+  resource_group_name = "${var.resource_group}"
 }
 
 resource "azurerm_subnet" "fwnet" {
   name                 = "AzureFirewallSubnet"
-  resource_group_name = ${var.resource_group}
+  resource_group_name = "${var.resource_group}"
   virtual_network_name = azurerm_virtual_network.net.name
   address_prefixes     = ["192.168.140.0/24"]
 }
 
 resource "azurerm_subnet" "acinet" {
   name                 = "aci-subnet"
-  resource_group_name = ${var.resource_group}
+  resource_group_name = "${var.resource_group}"
   virtual_network_name = azurerm_virtual_network.net.name
   address_prefixes     = ["192.168.141.0/24"]
 }
