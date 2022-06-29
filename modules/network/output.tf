67e3abd5421f6312c941d586d81426d5aad2fa96
Ivan <ivan.doskochynskyi@gmail.com>
2022-06-29 13:50:37 +0300

    add fw module

--- modules/network/output.tf
+++ modules/network/output.tf
@@ -1,16 +1,16 @@
 output "vnet_name" {
-  value azurerm_virtual_network.net.net
+  value = azurerm_virtual_network.net.name
 }
 
-output "fwnet"{
-  value azurerm_subnet.fwnet.name
+output "fwnet" {
+  value = azurerm_subnet.fwnet.name
 }
 
 output "fwnet_id" {
-  value azurerm_subnet.fwnet.id
+  value = azurerm_subnet.fwnet.id
 }
 
 output "acinet" {
-  value azurerm_subnet.acinet.name
+  value = azurerm_subnet.acinet.name
 }
 
