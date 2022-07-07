a6476e7df4a1f1290457df2b0fb3a18578283fc3
Ivan <73355620+doskochynskyi@users.noreply.github.com>
2022-07-07 21:19:31 +0300

    Update variables.tf

--- modules/network/variables.tf
+++ modules/network/variables.tf
@@ -5,3 +5,24 @@ variable "region" {
 variable "resource_group" {
   description = "This is the resource group where resources will be deployed."
 }
+
+variable "prefix" {
+  description = "This is the environment where your webapp is deployed. qa, prod, or dev"
+  #default = "prod"
+}
+
+variable "vnet_address_space" {
+  
+  description = "This is the environment where your webapp is deployed. qa, prod, or dev"
+  default = "192.168.0.0/16"
+}
+
+variable "subnet_firewall_address_space" {
+  description = "This is the environment where your webapp is deployed. qa, prod, or dev"
+  default = "192.168.171.0/24"
+}
+
+variable "subnet_aci_address_space" {
+  description = "This is the environment where your webapp is deployed. qa, prod, or dev"
+  default = "192.168.172.0/24"
+}
